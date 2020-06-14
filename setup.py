109cf02dbd5d0d953009a1be54b9cbdeb4a595fb
Shuhei Kishi <marubontan@gmail.com>
2020-06-13 23:08:03 -0300

    add setup

--- setup.py
+++ setup.py
@@ -2,6 +2,11 @@ from setuptools import setup
 
 setup(
     name="elizabeth",
-    version="1.0.0",
-    packages=['elizabeth'],
+    version="0.0.1",
+    author="Shuhei Kishi",
+    author_email="marubontan@gmail.com",
+    description=("Machine leaning pipeline"),
+    license="MIT",
+    url="https://github.com/marubontan/elizabeth",
+    packages=['elizabeth']
 )
