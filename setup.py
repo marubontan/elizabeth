305e40435e0b96e48be24a1c90d552e8fa5ad6d6
Shuhei Kishi <marubontan@gmail.com>
2020-06-30 20:45:53 -0300

    version up

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="elizabeth",
-    version="0.0.1",
+    version="0.0.2",
     author="Shuhei Kishi",
     author_email="marubontan@gmail.com",
     description=("Machine leaning pipeline"),
